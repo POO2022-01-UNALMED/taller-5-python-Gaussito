24e9ffcf06f50b75668b5a84981d5e10c5b06a2a
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-04-26 23:40:01 -0500

    me cago en el formateo de PyCharm, lo amoo

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,6 +1,4 @@
-
 class Animal:
-
     _totalAnimales = 0
 
     # CONSTRUCTOR
@@ -62,19 +60,10 @@ class Animal:
         import zooAnimales.anfibio
         import zooAnimales.pez
 
-        return f"""
-            Mamiferos : {zooAnimales.mamifero.Mamifero.cantidadMamiferos()}
-            Aves : {zooAnimales.ave.Ave.cantidadAves()}
-            Reptiles : {zooAnimales.reptil.Reptil.cantidadReptiles()}
-            Peces : {zooAnimales.pez.Pez.cantidadPeces()}
-            Anfibios : {zooAnimales.anfibio.Anfibio.cantidadAnfibios()}
-            """
+        return f"Mamiferos : {zooAnimales.mamifero.Mamifero.cantidadMamiferos()}\nAves : {zooAnimales.ave.Ave.cantidadAves()}\nReptiles : {zooAnimales.reptil.Reptil.cantidadReptiles()}\nPeces : {zooAnimales.pez.Pez.cantidadPeces()}\nAnfibios : {zooAnimales.anfibio.Anfibio.cantidadAnfibios()}\n"
 
     def __str__(self):
         if self._zona is None:
-            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi " \
-                   f"genero es {self._genero}"
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
         else:
-            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi " \
-                   f"genero es {self._genero}, la zona en la que me ubico es {self._zona.getNombre()}," \
-                   f" en el {self._zona.getZoo().getNombre()}"
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona.getNombre()}, en el {self._zona.getZoo().getNombre()}genero es {self._genero}, la zona en la que me ubico es {self._zona.getNombre()}, en el {self._zona.getZoo().getNombre()}"
